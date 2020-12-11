41947c1474f22edbf780160279580890b082b568
HJ SEO <hyunju6889@naver.com>
2020-12-11 20:49:44 +0900

    total
    Former-commit-id: 98297410f66fa762d6ec0a36eb2d9869d868efbc

---
+++ total/Assets/rayCast_dragon.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rayCast_dragon : MonoBehaviour
+{
+    Animator anim;
+    public TextMesh obj_text_mesh_;
+
+    public int atkPnt;
+    public int hltPnt;
+    float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj_text_mesh_.text = "체력:" + hltPnt;
+
+        anim = transform.GetComponent<Animator>();
+        atkPnt = 1000;
+        hltPnt = 5000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        obj_text_mesh_.text = "체력:" + hltPnt;
+
+        //forward 라는 이름의 방향변수 선언 및 대입
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 1000;
+        Debug.Log("jo");
+
+        //가상의 레이인 레이캐스트를 테스트 중에는 보일 수 있도록 함
+        Debug.DrawRay(transform.position, forward, Color.green);
+        //만약 레이케스트가 어떠한 물체에 맞는다면 hit라는 문구를 냄
+        if (Physics.Raycast(transform.position, forward, out hit))
+        {
+            Debug.Log("hit");
+            anim.SetBool("isHit", true);
+            timeElapsed = timeElapsed + Time.deltaTime;
+            if (timeElapsed >= 3)
+            {
+                hit.transform.GetComponent<rayCast_robot>().hltPnt = hit.transform.GetComponent<rayCast_robot>().hltPnt - atkPnt;
+                timeElapsed = 0;
+            }
+        }
+        else
+        {
+            anim.SetBool("isHit", false);
+
+        }
+        if (hltPnt <= 0) {
+            anim.SetBool("isDead", true);
+        }
+    }
+}
