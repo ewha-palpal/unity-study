256442add201f256aae1425d453a8683926d4f0d
HJ SEO <hyunju6889@naver.com>
2020-12-11 18:59:48 +0900

    total
    Former-commit-id: 30378a35c981cd5f4706bb0bef038bb117fadd0c

---
+++ total/Assets/Resources/VButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Events;
+
+public class VButton : MonoBehaviour
+{
+    VirtualButtonBehaviour[] virtualBtnBehaviours;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualBtnBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i < virtualBtnBehaviours.Length; ++i)
+        {
+            virtualBtnBehaviours[i].RegisterOnButtonPressed(OnButtonPressed);
+            virtualBtnBehaviours[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonPressed: " + vb.VirtualButtonName);
+        GetComponentInChildren<Animator>().SetBool("isAttack", true);
+        //warning.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonReleased: " + vb.VirtualButtonName);
+        GetComponentInChildren<Animator>().SetBool("isAttack", false);
+    }
+}
